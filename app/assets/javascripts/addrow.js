4be86a7c26b004897566184c12bb414261df7919
Michael <Michael.ginalick@gmail.com>
2015-09-01 22:25:44 -0500

    working on deleting rows from dom

--- app/assets/javascripts/addrow.js
+++ app/assets/javascripts/addrow.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
+	var counter = 2;
 
 	$('#add_row').on('click', function(){
-			$('#new_row').append('<div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Add" id="add_row" /></div>')
+			$('#new_row').append('<div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" id="new_row" /><label>Textbox #'+ counter + ' : </label>')
+			counter++;
 	});
+
+	 $(document).on("click",function(e){
+	 	e.preventDefault();
+	 	console.log("I am here");
+	 	//$(this).parent('div')remove();
+	 })
 
 });
