1c671c1264c8b06188184f4387aa5a182ccc665a
Michael <Michael.ginalick@gmail.com>
2015-09-02 00:19:46 -0500

    added spaces between text boxes

--- app/assets/javascripts/addrow.js
+++ app/assets/javascripts/addrow.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-	// var counter = 2;
-
-	// $('#add_row').on('click', function(){
-	// 		$('#new_row').append('<div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" id="new_row" /></div></p>')
-	// });
-
-	//  $(document).on("click",function(e){
-	//  	e.preventDefault();
-	//  	console.log("I am here");
-	//  	//$(this).parent('div')remove();
-	//  })
-
 
 	$(function () {
     $("#btnAdd").bind("click", function () {
@@ -23,11 +11,11 @@ $(document).ready(function(){
     $("body").on("click", ".remove", function () {
         $(this).closest("div").remove();
     });
-});
-function GetDynamicTextBox(value) {
+	});
+	function GetDynamicTextBox(value) {
     return '<name = "DynamicTextBox" type="text" value = "' + value + '" />&nbsp;' +
-            '<value="Remove" class="remove" <div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" /></div></p>'
-}
+            '<value="Remove" class="remove" <div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><br><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" /></div></p>'
+	}
 
 
 });
