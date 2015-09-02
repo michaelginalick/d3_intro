c1118cc43ad9efeecdcc22a59e65e0271db284f0
Michael <Michael.ginalick@gmail.com>
2015-09-01 20:51:26 -0500

    d3 graph on page

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,7 +1,7 @@
 $.ajax({
            type: "GET",
            contentType: "application/json; charset=utf-8",
-           url: 'data',
+           url: 'graph/data',
            dataType: 'json',
            success: function (data) {
                draw(data);
