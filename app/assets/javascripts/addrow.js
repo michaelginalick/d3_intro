91b170e48c392d32e75c6c6a8f2292b38333fcd9
Michael <Michael.ginalick@gmail.com>
2015-09-02 00:14:01 -0500

    adding and deleting rows via jquery function

--- app/assets/javascripts/addrow.js
+++ app/assets/javascripts/addrow.js
@@ -14,24 +14,19 @@ $(document).ready(function(){
 
 	$(function () {
     $("#btnAdd").bind("click", function () {
+    	//console.log("I am here");
         var div = $("<div />");
         div.html(GetDynamicTextBox(""));
         $("#TextBoxContainer").append(div);
     });
-    $("#btnGet").bind("click", function () {
-        var values = "";
-        $("input[name=DynamicTextBox]").each(function () {
-            values += $(this).val() + "\n";
-        });
-        alert(values);
-    });
+
     $("body").on("click", ".remove", function () {
         $(this).closest("div").remove();
     });
 });
 function GetDynamicTextBox(value) {
-    return '<input name = "DynamicTextBox" type="text" value = "' + value + '" />&nbsp;' +
-            '<input type="button" value="Remove" class="remove" />'
+    return '<name = "DynamicTextBox" type="text" value = "' + value + '" />&nbsp;' +
+            '<value="Remove" class="remove" <div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" /></div></p>'
 }
 
 
