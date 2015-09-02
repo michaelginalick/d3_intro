2c9916f4b4855d3c4ec1e5f5fe2e960039e34c50
Michael <Michael.ginalick@gmail.com>
2015-09-01 21:30:48 -0500

    can append new rows with js function

--- app/assets/javascripts/addrow.js
+++ app/assets/javascripts/addrow.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$('#add_row').on('click', function(){
-			console.log("I am here")
+			$('#new_row').append('<div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Add" id="add_row" /></div>')
 	});
 
 });
