0c266d42253e34eb59db3b12448d28e19951b37e
Michael <Michael.ginalick@gmail.com>
2015-09-07 18:12:55 -0500

    added new js file for reset function

---
+++ app/assets/javascripts/reset_graph.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("#reset_graph").click(function(){
+		console.log("I am here");
+	})
+});
--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -3,7 +3,6 @@ class GraphController < ApplicationController
   end
  
   def data
-  	puts "I am here" + params[:values].to_s
     respond_to do |format|
       format.json {
         render :json => params[:values]
