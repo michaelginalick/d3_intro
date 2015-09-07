83bb3d9756f1cda7f0d801cd70d755db1b0e8124
Michael <Michael.ginalick@gmail.com>
2015-09-07 12:21:29 -0500

    added ajax call to gen graph

--- app/assets/javascripts/gengraph.js
+++ app/assets/javascripts/gengraph.js
@@ -3,5 +3,23 @@ $(document).ready(function(){
         $("#genGraph").click(function(e){
             e.preventDefault();
             console.log("I am here");
+            genGraph();
         })
 });
+
+
+
+var genGraph = function() { 
+        $.ajax({
+           type: "GET",
+           contentType: "application/json; charset=utf-8",
+           url: 'graph/data',
+           dataType: 'json',
+           success: function (data) {
+               draw(data);
+           },
+           error: function (result) {
+               error();
+           }
+       });
+    }
