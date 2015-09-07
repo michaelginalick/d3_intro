3c803bc3bc0d0521f7dafc2e42d741dc0ae5c317
Michael <Michael.ginalick@gmail.com>
2015-09-07 13:46:04 -0500

    fixed js bug

--- app/assets/javascripts/addrow.js
+++ app/assets/javascripts/addrow.js
@@ -8,13 +8,12 @@ $(document).ready(function(){
     });
 
         $(document).on("click","#remove", function () {
-            console.log("I am here");
             $(this).closest("div").remove();
         });
 	});
 	function GetDynamicTextBox(value) {
     return '<name = "DynamicTextBox" type="text" value = "' + value + '" />&nbsp;' +
-            '<value="Remove" class="remove" id="remove" <div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><br><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" /></div></p>'
+            '<div class="row"><div class="col-md-6 col-md-offset-3"><div class="form-group"><br><input type="text" class="form-control" /></div><input type="submit" class="btn btn-primary" value="Remove" id="remove"/></div></p>'
 	}
 
 
