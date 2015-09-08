6f8f66c848265f83cd28eab0ec7fad4ef34dde57
Michael <Michael.ginalick@gmail.com>
2015-09-08 13:14:22 -0500

    reset text box on reset

--- app/assets/javascripts/reset_graph.js
+++ app/assets/javascripts/reset_graph.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 	$("#reset_graph").click(function(){
 		reset();
+		clear();
 	})
 
 
 //loop through text boxes and remove all but the first one
 	var reset = function() {
-			var num = $("input:text").length;
-			while (num >= 1) {
-						var ele = $("#remove");
-            ele.closest("div").remove();
-            num--;
-        }
-			}
+		var num = $("input:text").length;
+		while (num >= 1) {
+			var ele = $("#remove");
+      ele.closest("div").remove();
+      num--;
+    }
+	}
+
+	var clear = function() {
+		var text = $("input:text");
+		text.val(" ");
+	}
+
 	});
