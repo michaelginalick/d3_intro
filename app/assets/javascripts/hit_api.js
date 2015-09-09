ec784bed25a222c84bbcd675e422c58163565283
Michael <Michael.ginalick@gmail.com>
2015-09-08 22:19:26 -0500

    js formatting

--- app/assets/javascripts/hit_api.js
+++ app/assets/javascripts/hit_api.js
@@ -1,24 +1,24 @@
 
-    function hitApi() {
-        console.log("I am here");
-        $.ajax({
-            type: "GET",
-            url: 'http://api.fixer.io/latest?base=USD',
-            dataType: 'json',
-            success: function(data){
-                console.log(data);
-            },
-            error: function(result){
-                error();
-            }
-        });
-    };
+function hitApi() {
+    console.log("I am here");
+    $.ajax({
+        type: "GET",
+        url: 'http://api.fixer.io/latest?base=USD',
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
+        },
+        error: function(result){
+            error();
+        }
+    });
+};
 
 
-    function dropDownValue() {
-        var value = $(".btn:first-child").val($(this).text());
-        console.log(value);
-        return value;
-    }
+function dropDownValue() {
+    var value = $(".btn:first-child").val($(this).text());
+    console.log(value);
+    return value;
+}
 
     
