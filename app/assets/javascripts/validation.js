024a9170f6c071ea5e5f7d43b7cae8a0ad271aa1
Michael <Michael.ginalick@gmail.com>
2015-09-10 21:17:08 -0500

    added input validation

---
+++ app/assets/javascripts/validation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$(document).keypress('.form-control',function(event){
+			if(event.which != 8 && isNaN(String.fromCharCode(event.which)) || event.keycode == 32) {
+				event.preventDefault();
+			};
+	})
+
+});
